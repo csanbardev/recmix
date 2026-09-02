76dca71ea42a9d728d02788a861b8bb0d8b598d3
csanbardev <csanbar97@gmail.com>
2026-09-02 18:40:45 +0200

    solve date bug

--- src/models/recipesList.model.js
+++ src/models/recipesList.model.js
@@ -2,7 +2,7 @@ import { pool } from "../db.js";
 
 export const getLastRecipesList = async () => {
   try {
-    const [rows] = await pool.query("select * from t_recipes_list where recl_fec in (select max(recl_fec) from t_recipes_list)");
+    const [rows] = await pool.query("select DATE_FORMAT(recl_fec, '%Y-%m-%dT%H:%i:%s') AS recl_fec, recl_rec_id from t_recipes_list where recl_fec in (select max(recl_fec) from t_recipes_list)");
 
     if (rows === null || rows.length <= 0) {
       throw new Error("404");
