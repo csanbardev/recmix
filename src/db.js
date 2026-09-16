cbf6908bf46f1b2c0c0bc1a04f8c216f23e13ff3
csanbardev <csanbar97@gmail.com>
2026-09-16 14:51:28 +0200

    create config for db

--- src/db.js
+++ src/db.js
@@ -1,3 +1,4 @@
+import fs from 'node:fs'
 import { createPool } from "mysql2/promise";
 
 import{
@@ -5,13 +6,20 @@ import{
   DB_PORT,
   DB_DATABASE,
   DB_PASSWORD,
-  DB_USER
+  DB_USER,
+  DB_SSL,
+  DB_SSL_CA
 } from './config.js'
+
+const sslConfig = DB_SSL
+  ? (DB_SSL_CA ? { ca: fs.readFileSync(DB_SSL_CA), rejectUnauthorized: false } : { rejectUnauthorized: false })
+  : false
 
 export const pool = createPool({
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASSWORD,
   port: DB_PORT,
-  database: DB_DATABASE
+  database: DB_DATABASE,
+  ssl: sslConfig
 })
